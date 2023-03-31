9e28d27924ab20b9f519deb6551d16bc456577da
Princen <c14200181@john.petra.ac.id>
2023-03-31 09:41:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'first-app';
+var CACHE_NAME = 'Portofolio';
 var urlsToCache = [
   '/',
   '/index.html',
@@ -8,9 +8,41 @@ var urlsToCache = [
   '/portfolio-example01.html',
   '/styles.css',
   '/app.js',
-  '/images/*',
-  '/Tutorial/*'
+  '/sw.js',
+  '/manifest.json',
+  '/images/logo.png',
+  '/images/about-header.jpg',
+  '/images/contact-image.jpg',
+  '/images/example-blog01.jpg',
+  '/images/example-blog02.jpg',
+  '/images/example-blog03.jpg',
+  '/images/example-blog04.jpg',
+  '/images/example-blog05.jpg',
+  '/images/example-blog06.jpg',
+  '/images/example-blog07.jpg',
+  '/images/footer-background.png',
+  '/images/header-bg.jpg',
+  '/images/logo.png',
+  '/images/photo.jpg',
+  '/images/photo-wide.jpg',
+  '/images/portfolio-example-01.jpg',
+  '/images/portfolio-example-02.jpg',
+  '/images/portfolio-example-03.jpg',
+  '/images/portfolio-example-04.jpg',
+  '/images/portfolio-example-05.jpg',
+  '/images/portfolio-example-06.jpg',
+  '/images/example-work01.jpg',
+  '/images/example-work07.jpg',
+  '/images/example-work02.jpg',
+  '/images/example-work03.jpg',
+  '/images/example-work04.jpg',
+  '/images/example-work05.jpg',
+  '/images/example-work06.jpg',
+  '/images/example-work08.jpg',
+  '/images/example-work09.jpg'
 ];
+
+
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -51,3 +83,22 @@ self.addEventListener('fetch', function(event) {
       })
   );
 });
+
+// Aktifkan service worker dan hapus cache yang sudah kadaluarsa
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+
