534294397e51549756feec34132a523d6fac69a5
Princen <c14200181@john.petra.ac.id>
2023-03-31 09:25:08 +0800

    update2

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var title = document.querySelector('.title');
-var courseFeatureElements = document.querySelectorAll('.course-feature');
-var button = document.querySelector('button');
-
-navigator.serviceWorker.register('/sw.js');
+navigator.serviceWorker.register('sw.js');
 
 
